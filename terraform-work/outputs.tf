e3e9a232afd874b1804aa970fcec25ec8ca28eed
vishal-salgia <vishal.salgia@intellibridge.us>
2022-11-16 19:13:21 -0500

    corrected the local_file resouce

--- terraform-work/outputs.tf
+++ terraform-work/outputs.tf
@@ -9,11 +9,11 @@ output "jenkins_agent_ip" {
 resource "local_file" "ansible_inventory" {
     count = 4
     content = "[Jenkins_Master]\n ${
-        jenkins_master_ip.value
+        vsphere_virtual_machine.vm_jenkins_master.default_ip_address
     }\n[Jenkins_Agent]\n${join("\n",
             formatlist(
-              "%s",              
-              jenkins_agent_ip.value
+              "%s",
+              vsphere_virtual_machine.vm_jenkins_agent.*.default_ip_address
             )
         )}"
 
