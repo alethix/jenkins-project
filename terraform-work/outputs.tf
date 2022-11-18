18ebbe4f07c7c878c0a3892dc4a35679c74b3242
vishal-salgia <vishal.salgia@intellibridge.us>
2022-11-17 21:05:23 -0500

    Added ansible work and also modified terraform outputs.tf

--- terraform-work/outputs.tf
+++ terraform-work/outputs.tf
@@ -8,9 +8,9 @@ output "jenkins_agent_ip" {
 
 resource "local_file" "ansible_inventory" {
     count = 4
-    content = "[Jenkins_Master]\n${
+    content = "[jenkins_master]\n${
         vsphere_virtual_machine.vm_jenkins_master.default_ip_address
-    }\n[Jenkins_Agent]\n${join("\n",
+    }\n[jenkins_agent]\n${join("\n",
             formatlist(
               "%s",
               vsphere_virtual_machine.vm_jenkins_agent.*.default_ip_address
